d46bade64f793772c8f6a3c88171f7e70fe312ef
SkimoCHK <alexguillen265@gmail.com>
2024-01-22 09:17:24 -0700

    Agregar archivos de proyecto.

---
+++ CALCU_MVVM_AGGP/CALCU_MVVM_AGGP/ViewModel/VMpage1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CALCU_MVVM_AGGP.ViewModel
+{
+    public class VMpage1 : BaseViewModel
+    {
+        #region VARIABLES
+        private double _Valor1;
+        private double _Valor2;
+        private double _Valor3;
+        private string _Label1;
+        private string _Label2;
+        private bool _LimpiarPantalla;
+        #endregion
+
+        #region CONSTRUCTOR
+        public VMpage1(INavigation navigation) 
+        {
+            Navigation = navigation;
+        }
+        #endregion
+
+        #region OBJETOS
+        public double Valor1
+        {
+            get { return _Valor1;  }
+            set { SetValue(ref _Valor1, value); }
+        }
+        public double Valor2
+        {
+            get { return _Valor2; }
+            set { SetValue(ref _Valor2, value);}
+        }
+        public double Valor3
+        {
+            get { return _Valor3; }
+            set { SetValue(ref _Valor3, value); }
+            
+        }
+
+        #endregion
+
+        #region PROCESOS
+
+        #endregion
+
+        #region COMANDOS
+        #endregion
+
+    }
+}
---
+++ CALCU_MVVM_AGGP/CALCU_MVVM_AGGP/Views/Page1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CALCU_MVVM_AGGP.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CALCU_MVVM_AGGP.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new VMpage1(Navigation);
+        }
+    }
+}
