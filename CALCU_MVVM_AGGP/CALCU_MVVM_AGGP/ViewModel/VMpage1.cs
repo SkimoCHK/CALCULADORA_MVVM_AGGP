1894748d05f208b9772ba2e94303fcd2d9db0d43
SkimoCHK <alexguillen265@gmail.com>
2024-01-23 17:39:37 -0700

    uchala la mama ya funciona

--- CALCU_MVVM_AGGP/CALCU_MVVM_AGGP/ViewModel/VMpage1.cs
+++ CALCU_MVVM_AGGP/CALCU_MVVM_AGGP/ViewModel/VMpage1.cs
@@ -1,65 +1,72 @@
-﻿using System.Windows.Input;
+﻿using System.Linq;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace CALCU_MVVM_AGGP.ViewModel
 {
     public class VMpage1 : BaseViewModel
     {
-        #region VARIABLES
         private double _valor1;
         private double _valor2;
         private string _operador;
         private bool _limpiarPantalla;
         private string _resultadoTexto;
         private string _operacionTexto;
-        #endregion
-        #region CONSTRUCTOR
+
+
         public VMpage1(INavigation navigation)
         {
             Navigation = navigation;
-            _resultadoTexto = "0";
-            _operacionTexto = "";
+            ResetearCalculadora();
         }
-        #endregion
-        #region OBJETOS
+
         public double Valor1
         {
-            get { return _valor1; }
-            set { SetValue(ref _valor1, value); }
+            get => _valor1;
+            set => SetValue(ref _valor1, value);
         }
 
         public double Valor2
         {
-            get { return _valor2; }
-            set { SetValue(ref _valor2, value); }
+            get => _valor2;
+            set => SetValue(ref _valor2, value);
         }
 
         public string ResultadoTexto
         {
-            get { return _resultadoTexto; }
-            set { SetValue(ref _resultadoTexto, value); }
+            get => _resultadoTexto;
+            set => SetValue(ref _resultadoTexto, value);
         }
 
         public string OperacionTexto
         {
-            get { return _operacionTexto; }
-            set { SetValue(ref _operacionTexto, value); }
+            get => _operacionTexto;
+            set => SetValue(ref _operacionTexto, value);
         }
-        #endregion
 
-        #region PROCESOS
+        public ICommand NumeroCommand => new Command<string>(Numero);
+        public ICommand OperacionCommand => new Command<string>(Operacion);
+        public ICommand IgualCommand => new Command(Igual);
+        public ICommand LimpiarPantallaCommand => new Command(ResetearCalculadora);
+        public ICommand BorrarUnNumeroCommand => new Command(BorrarUnNumero);
+
         private void Numero(string numero)
         {
- 
             if (_limpiarPantalla)
             {
+                // No limpiar completamente, solo reiniciar el número actual
                 ResultadoTexto = "0";
-                OperacionTexto = "";  
                 _limpiarPantalla = false;
             }
 
-            if (ResultadoTexto == null)
+            if (!string.IsNullOrEmpty(_operacionTexto) && "+-x/".Contains(_operacionTexto.Last()))
             {
+                _operacionTexto += " ";
+            }
+
+            if (ResultadoTexto == "0" && numero != ".")
+            {
+                
                 ResultadoTexto = numero;
             }
             else if (!ResultadoTexto.Contains(".") || numero != ".")
@@ -67,34 +74,30 @@ namespace CALCU_MVVM_AGGP.ViewModel
                 ResultadoTexto += numero;
             }
 
-            OperacionTexto += numero;
-            
+            _operacionTexto += numero;
         }
 
 
         private void Operacion(string operador)
         {
-
             if (!string.IsNullOrEmpty(ResultadoTexto))
             {
-                if (!string.IsNullOrEmpty(_operador) && !_limpiarPantalla)
+                if (!_limpiarPantalla)
                 {
                     Calcular();
+                    Valor1 = double.Parse(ResultadoTexto);
+                    OperacionTexto = $"{Valor1} {_operador}";
                 }
 
-                _valor1 = double.Parse(ResultadoTexto);
                 _operador = operador;
                 _limpiarPantalla = true;
-
-                OperacionTexto = $"{_valor1}{_operador}";
+                OperacionTexto = $"{Valor1} {_operador}";
             }
             else
             {
                 ResultadoTexto = "Error";
             }
-
         }
-
 
 
         private void Igual()
@@ -103,12 +106,12 @@ namespace CALCU_MVVM_AGGP.ViewModel
             _operador = "";
         }
 
-        private void LimpiarPantalla()
+        private void ResetearCalculadora()
         {
             ResultadoTexto = "0";
             OperacionTexto = "";
-            _valor1 = 0;
-            _valor2 = 0;
+            Valor1 = 0;
+            Valor2 = 0;
             _operador = "";
         }
 
@@ -118,7 +121,8 @@ namespace CALCU_MVVM_AGGP.ViewModel
             {
                 ResultadoTexto = ResultadoTexto.Substring(0, ResultadoTexto.Length - 1);
             }
-            if (ResultadoTexto == "")
+
+            if (string.IsNullOrEmpty(ResultadoTexto))
             {
                 ResultadoTexto = "0";
             }
@@ -128,13 +132,14 @@ namespace CALCU_MVVM_AGGP.ViewModel
         {
             if (double.TryParse(ResultadoTexto, out double resultadoNumerico))
             {
-                _valor2 = resultadoNumerico;
-                ResultadoTexto = RealizarOperacion(_valor1, _valor2, _operador).ToString();
-                _limpiarPantalla = true;
+                Valor2 = resultadoNumerico;
+
+                // Utilizamos el operador almacenado en "_operador" en lugar de "+"
+                ResultadoTexto = RealizarOperacion(Valor1, Valor2, _operador).ToString();
+                _limpiarPantalla = false;
             }
             else
             {
-    
                 ResultadoTexto = "Error";
             }
         }
@@ -151,43 +156,10 @@ namespace CALCU_MVVM_AGGP.ViewModel
                 case "x":
                     return num1 * num2;
                 case "/":
-                    if (num2 != 0)
-                    {
-                        return num1 / num2;
-                    }
-                    else
-                    {
-                        return double.NaN;
-                    }
+                    return num2 != 0 ? num1 / num2 : double.NaN;
                 default:
                     return num2;
             }
         }
-        #endregion
-
-        #region COMANDOS
-
-        public ICommand NumeroCommand => new Command<string>(Numero);
-        public ICommand OperacionCommand => new Command<string>(Operacion);
-        public ICommand IgualCommand => new Command(Igual);
-        public ICommand LimpiarPantallaCommand => new Command(LimpiarPantalla);
-        public ICommand BorrarUnNumeroCommand => new Command(BorrarUnNumero);
-
-
-        public ICommand NumeropuntoCommand => new Command(() => Numero("."));
-        public ICommand Numero00Command => new Command(() => Numero("00"));
-        public ICommand Numero0Command => new Command(() => Numero("0"));
-        public ICommand Numero1Command => new Command(() => Numero("1"));
-        public ICommand Numero2Command => new Command(() => Numero("2"));
-        public ICommand Numero3Command => new Command(() => Numero("3"));
-
-        public ICommand Numero4Command => new Command(() => Numero("4"));
-        public ICommand Numero5Command => new Command(() => Numero("5"));
-        public ICommand Numero6Command => new Command(() => Numero("6"));
-
-        public ICommand Numero7Command => new Command(() => Numero("7"));
-        public ICommand Numero8Command => new Command(() => Numero("8"));
-        public ICommand Numero9Command => new Command(() => Numero("9"));
-        #endregion
     }
 }
